c48d5f658f35eb67b12a7d98e5b27443e71edf5b
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2021-03-11 12:14:46 +0400

    Add API key with .env

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Main from './components/layout/Main';
 import Footer from './components/layout/Footer';
 
 function App() {
+  //API fetch
+  // http://www.omdbapi.com/?s=star%20wars&apikey=xxxxxx
+  // const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=${process.env.REACT_APP_OMDB_API_KEY}`;
+
   return (
     <div className=''>
       <Header />
